dbde0c2ca68ef696355ff0df06d9905e3399ced2
stan-005 <agotistanley005@gmail.com>
2024-05-07 16:44:36 +0300

    javacript app

---
+++ app.js
@@ -0,0 +1,8 @@
+// Prompt the user for their name
+var name = prompt("What's your name?");
+
+// Construct the greeting message
+var greeting = "Hello, " + name + "!";
+
+// Display the greeting message
+alert(greeting);
